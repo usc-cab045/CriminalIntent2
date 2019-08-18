b103bc1f9b14913f5b3c76f18893ba9857bf869f
Courtenay Brown <cab045@student.usc.edu.au>
2019-08-18 10:39:55 +1000

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package android.bignerdranch.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+
+
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
